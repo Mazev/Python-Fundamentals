bae6a1c7cf8af742639be6834eb3d55c08344553
Mazev <77510376+Mazev@users.noreply.github.com>
2021-01-26 22:18:30 +0200

    new

---
+++ 12.Lists Basics More Exercises.py/03. Josephus Permutation.py
@@ -0,0 +1,17 @@
+permutation = input().split()
+kill_count = int(input())
+
+new_list = []
+counter = 0
+
+index = 0
+while len(permutation) > 0:
+    counter +=1
+    if counter % kill_count == 0:
+        new_list.append(permutation.pop(index))
+    else:
+        index +=1
+    if index >= len(permutation):
+        index = 0
+
+print(new_list)
